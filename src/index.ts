0f9c2010361d586b30067adf83f2dd9b2eefc14d
Myoui-sys <meninagamer55@gmail.com>
2026-09-14 16:25:04 -0300

    adiciona definição do tipo Gato e instancia gato1 no projeto

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,22 @@ function somar(a: number = 0, b: number = 0): number {
 
 const resultado: number = somar();
 console.log(resultado);
+
+type Gato = {
+  nome: string;
+  idade: number;
+  vacina: boolean;
+  raca: string;
+  sexo: string;
+  cor: string;
+}
+
+const gato1: Gato = {
+  nome: "Mero",
+  idade: 7,
+  vacina: true,
+  raca: "SRD",
+  sexo: "F",
+  cor: "Tricolor"
+}
+
